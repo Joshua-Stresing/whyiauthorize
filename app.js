754d43b5f6544561a48cb02d18cd6e71ee3927c9
Joshua Stresing <pyrotorch69@yahoo.com>
2022-02-14 12:38:26 -0800

    first ones working?

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { signupUser, signInUser, redirectIfLoggedIn, getUser } from './fetch-utils.js';
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
@@ -6,6 +7,14 @@ const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
 
+const user = await getUser();
+
 // Wire up sign in and sign up forms to supabase
+signUpForm.addEventListener('submit', async (event)=>{
+    event.preventDefault();
+    console.log(signInEmail.value, signUpPassword.value);
+    const user = await signupUser(signUpEmail.value, signUpPassword.value);
+});
 // Redirect to /other-page on successful auth
+
 // Redirect to /other-page when page loads if user is authenticated
